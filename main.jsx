c82a2aa9987c880e19dacaccb74c653b098505c5
Gaurav Chaudhari <156290730+Gaurav-Chaudhari-1012@users.noreply.github.com>
2024-03-03 21:50:51 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+//import "./globle.css"
+
+// console.log(React);
+// console.log(ReactDOM);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
