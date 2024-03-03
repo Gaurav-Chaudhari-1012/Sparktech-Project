851819332bd3a1619841afe7b87cfe4536b79aa6
Gaurav Chaudhari <156290730+Gaurav-Chaudhari-1012@users.noreply.github.com>
2024-03-03 21:50:18 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './HomePage/Navbar'
+import Footer from './HomePage/Footer'
+import Cards from './HomePage/Cards'
+
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Cards/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
